0b9b27e2159c86b7734077132d08c3be9bd08d56
kangtw <xodn95000@gamil.com>
2021-10-26 00:24:21 +0900

    react test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-import Test from './Component/test';
+import Test from './Component/test'; //만든 컴포넌트를 임포트 
+
+
+const test = [{
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',//보여주고싶은 랜덤이미지
+    'name': '강태우',
+    'birthday': '940216'
+},
+{
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',//보여주고싶은 랜덤이미지
+    'name': '강태우',
+    'birthday': '940216'
+}
+    ,
+{
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',//보여주고싶은 랜덤이미지
+    'name': '강태우',
+    'birthday': '940216'
+}]
 
 
 function App() {
     return (
-        <Test />
+        <div>
+            {test.map(c => {
+                return (<Test key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday}
+                />
+                )
+            })
+            }
+        </div>
     )
 }
 
--- src/Component/test.js
+++ src/Component/test.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React from "react"; //import : 특정 라이브러리를 불러온다. 
 
-class Test extends React.Component {
+
+class Test extends React.Component {  //리액트의 컴포넌트 형태로 작성된 클래스
+
     render() {
         return (
             <div>
-                <h1>hi</h1>
+                <TestProfile id={this.props.id} image={this.props.image} />
+                <TestInfo name={this.props.name} birthday={this.props.birthday} />
+            </div>
+
+
+        )
+    }
+}
+
+class TestProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.image} alt='profile' />
+                <h2>{this.props.id}</h2>
             </div>
         )
     }
 }
 
-export default Test;
+class TestInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.name}</h1>
+                <h1>{this.props.birthday}</h1>
+            </div>
+        )
+    }
+}
+
+export default Test;   //다른 컴포넌트에서 사용할 수 있도록 내보내기를 한다. 
